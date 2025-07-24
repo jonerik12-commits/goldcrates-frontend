b6bd4c13464d9a4116d39615b34f4d0c923a1a41
jonerik12-commits <jonerik12@gmail.com>
2025-07-24 14:08:55 -0500

    Update App.js

--- gold crates front end/src/App.js
+++ gold crates front end/src/App.js
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState('Connecting...');
 
   useEffect(() => {
-    fetch('https://goldcrates-801d48bd55f3.herokuapp.com/')
+    fetch('https://github.com/jonerik12-commits/goldcrates-frontend')
       .then((res) => res.text())
       .then((data) => setMessage(data))
       .catch(() => setMessage('Failed to connect to backend'));
